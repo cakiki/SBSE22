32e5071b04d0bf9dd48eb74268c1415fe81c720d
Christopher Akiki <christopher.akiki@uni-leipzig.de>
2022-07-21 17:26:53 +0200

    Basic candidate class working

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,10 @@ FROM python:3.10
 # Set the working directory to /app
 WORKDIR /app
 
-COPY . .
+COPY submission/requirements.txt submission/requirements.txt
 
 RUN python3 -m pip install -r submission/requirements.txt
 
-ENTRYPOINT exec python3 -m unittest tests1-simple.py tests2-extended.py
+COPY . .
+
+ENTRYPOINT exec python3 -m unittest -v tests1-simple.py tests2-extended.py
--- submission/generator.py
+++ submission/generator.py
@@ -1,5 +1,10 @@
 import inspect
+from itertools import chain
+import random
+import time
 from abc import ABC, abstractmethod
+from typing import Any, Dict, List, Tuple
+
 from fuzzingbook.Coverage import Coverage, branch_coverage
 
 
@@ -33,8 +38,114 @@ class AbstractCandidate(ABC):
         return NotImplemented
 
 
+class Candidate(AbstractCandidate):
+    def __init__(self, target_class: type, method_dict: Dict):
+        self.target_class = target_class
+        self.method_dict = method_dict
+        self.all_params = {}
+        self.genome_vec = None
+        self.method_calls = None
+        self.total_cost = None
+        self.total_coverage = None
+        self.total_code = None
+
+    def vectorize(self):
+        """
+        Converts method_args (the genome) into a vector to easily mutate
+        """
+        list_of_list = [i.values() for i in self.all_params.values()]
+        self.genome_vec = list(chain(*list_of_list))
+
+    def devectorize(self, vec):
+        """
+        Converts a vector into a human readable representation thats also usable for testing.
+        """
+        i = 0
+
+        for method_name, method_metadata in self.method_dict.items():
+            params = {}
+
+            for param_name in method_metadata["params"].keys():
+                params[param_name] = vec[i]
+                i += 1
+
+        self.all_params[method_name] = params
+
+    def gen_random(self):
+        for method_name, method_metadata in self.method_dict.items():
+            params = {}
+
+            for parameter_name, parameter_type in method_metadata["params"].items():
+                if parameter_type is int:
+                    params[parameter_name] = random.randint(-10, 10)
+                if parameter_type is float:
+                    params[parameter_name] = random.uniform(-10, 10)
+
+            self.all_params[method_name] = params
+
+    def get_cost(self) -> float:
+        """
+        returns cost in execution time
+        """
+        return self.total_cost
+
+    def get_coverage(self) -> int:
+        """
+        returns coverage as a number
+        """
+        return self.total_coverage
+
+    def run(self):
+        """
+        runs test and throws no error
+        """
+        self.total_cost = 0
+        self.total_coverage = 0
+        self.total_code = []
+
+        constructor_call = None
+        method_calls = []
+
+        for method_name, method_metadata in self.method_dict.items():
+            if method_name == "__init__":
+                constructor_call = MethodCall(
+                    method_name, method_metadata, self.all_params[method_name]
+                )
+            else:
+                method_calls.append(
+                    MethodCall(
+                        method_name, method_metadata, self.all_params[method_name]
+                    )
+                )
+
+        class_instance = constructor_call.run_constructor(self.target_class)
+        self.total_cost += constructor_call.execution_time_fitness()
+        self.total_coverage += constructor_call.branch_coverage_fitness()
+        self.total_code.append(constructor_call.get_line())
+
+        for method_call in method_calls:
+            method_call.run_method(class_instance)
+            self.total_cost += method_call.execution_time_fitness()
+            self.total_coverage += method_call.branch_coverage_fitness()
+            self.total_code.append(method_call.get_line())
+
+    def get_lines_of_code(self) -> str:
+        """
+        returns string that contains the lines of the test
+        """
+        return "\n\n".join(self.total_code)
+
+
+def generate_random_candidates(target_class: type, method_dict: Dict, n: int):
+    candidates = [Candidate(target_class, method_dict) for i in range(n)]
+    for candidate in candidates:
+        candidate.gen_random()
+        candidate.run()
+    return candidates
+
+
 class AbstractGenerator(ABC):
-    def generate(self, target_class: type):
+    def generate(self, target_class: type) -> List[AbstractCandidate]:
         """
         Runs MOO and returns the final solution set containing the pareto front
         Always uses doesitwork.py in the same directory. This file will be swapped out for evaluation.
@@ -46,14 +157,21 @@ class Generator(AbstractGenerator):
     def __init__(self):
         pass
 
-    def generate(self, target_class: type):
+    def generate(self, target_class: type) -> List[AbstractCandidate]:
         ###################################
         ### IMPLEMENT YOUR SOLUTION HERE ##
         ###################################
+        POP_SIZE = 10
+
+
+        method_dict = get_methods(target_class)
+        front = generate_random_candidates(target_class, method_dict, POP_SIZE)
+
         generation = 1
         while True:
             # select, breed, mutate
-            front = ["ConcreteCandidate1", "ConcreteCandidate2", "ConcreteCandidate3", ]
+            front = generate_random_candidates(target_class, method_dict, POP_SIZE)
+            # print(front)
             # yield to act like a iterable for interface as used in provided test cases
             yield front
             generation += 1
@@ -61,8 +179,13 @@ class Generator(AbstractGenerator):
 
 ##### EXAMPLE CODE TO GET STARTED
 
+
 def get_methods(class_ref):
-    methods = [method for method in dir(class_ref) if not method.startswith("__") or "__init__" in method]
+    methods = [
+        method
+        for method in dir(class_ref)
+        if not method.startswith("__") or "__init__" in method
+    ]
     method_dict = {}
     for method in methods:
         method_dict[method] = {"params": {}}
@@ -81,64 +204,99 @@ def get_methods(class_ref):
 class MethodCall:
     FAILED = "FAILED"
 
-    def __init__(self, method_name, method_metadata, ercs, lower, upper):
-        self.cov = None
-        self.m_dict = method_metadata
-        self.m_name = method_name
-        self.params = {}
-        self.lower = lower
-        self.upper = upper
+    def __init__(self, method_name: str, method_metadata: Dict, params: Dict) -> None:
+        self.coverage = None
+        self.execution_time = None
+        self.method_metadata = method_metadata
+        self.method_name = method_name
+        self.params = params
         self.result = None
-        for param, p_type in self.m_dict["params"].items():
-            # TODO: handle types here
-            print("Processing", p_type, "parameter")
-            self.params[param] = 5
+        # for parameter_name, parameter_type in self.method_metadata["params"].items():
+        #     # TODO: handle types here
+        #     print("Processing", parameter_type, "parameter")
+        #     self.params[parameter_name] = 5
 
-    def is_init(self):
-        return self.m_name == "__init__"
+    # def is_init(self) -> bool:
+    #     return self.method_name == "__init__"
 
-    def run(self, obj):
+    def run_constructor(self, constructor) -> Any:
+        """
+        super cursed
+        """
         args = {k: v for k, v in self.params.items()}
-        if self.is_init():
-            # obj is Class
-            with Coverage() as cov:
-                try:
-                    result = obj(**args)
-                except:
-                    result = MethodCall.FAILED
-        else:
-            with Coverage() as cov:
-                try:
-                    result = getattr(obj, self.m_name)(**args)
-                except:
-                    result = MethodCall.FAILED
-        self.result = result
-        del cov.original_trace_function
-        self.cov = cov
-        return cov, result
 
-    def run_raw(self, obj):
+        with Coverage() as coverage:
+            try:
+                start_time = time.time()
+                result = constructor(**args)
+            except:
+                result = MethodCall.FAILED
+
+        self.execution_time = time.time() - start_time
+        self.result = result
+        del coverage.original_trace_function
+        self.coverage = coverage
+        return self.result
+
+    def run_method(self, class_instance) -> Tuple[float, Coverage, Any]:
+        """
+        cursed
+        """
+        args = {k: v for k, v in self.params.items()}
+
+        with Coverage() as coverage:
+            try:
+                start_time = time.time()
+                result = getattr(class_instance, self.method_name)(**args)
+            except:
+                result = MethodCall.FAILED
+
+        self.execution_time = time.time() - start_time
+        self.result = result
+        del coverage.original_trace_function
+        self.coverage = coverage
+        return self.coverage, self.result
+
+    def run_raw(self, function_pointer):
         args = {k: v.get() for k, v in self.params.items()}
         if self.is_init():
             # obj is Class
-            result = obj(**args)
+            result = function_pointer(**args)
         else:
-            result = getattr(obj, self.m_name)(**args)
+            result = getattr(function_pointer, self.method_name)(**args)
         return result
 
-    def branch_coverage_fitness(self, ignore_methods=None):
-        trace = [(method, line) for method, line in self.cov.trace() if method not in ignore_methods]
+    def branch_coverage_fitness(self) -> int:
+        trace = [
+            (method, line)
+            for method, line in self.coverage.trace()
+            #  if method not in ignore_methods
+        ]
         pairs = branch_coverage(trace)
-        return len(pairs), pairs
+        return len(pairs)
 
-    def line_coverage_fitness(self, ignore_methods=None):
-        lines = [(method, line) for method, line in self.cov.coverage() if method not in ignore_methods]
-        return len(lines), lines
+    # def line_coverage_fitness(self, ignore_methods=None) -> int:
+    #     lines = [
+    #         (method, line)
+    #         for method, line in self.coverage.coverage()
+    #         if method not in ignore_methods
+    #     ]
+    #     return len(lines)
 
-    def get_line(self):
+    def execution_time_fitness(self) -> float:
+        return self.execution_time
+
+    def get_line(self) -> str:
         return self.__repr__()
 
-    def __repr__(self):
-        s = "{}({})".format(self.m_name,
-                            ", ".join(["{} = {}".format(p_name, p_val) for p_name, p_val in self.params.items()]))
+    def __repr__(self) -> str:
+        s = "{}({})".format(
+            self.method_name,
+            ", ".join(
+                [
+                    "{} = {}".format(p_name, p_val)
+                    for p_name, p_val in self.params.items()
+                ]
+            ),
+        )
         return s
